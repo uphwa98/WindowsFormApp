421bc37cf1d7264966ea11d1b04f5a69d39a7f13
uphwa98 <uphwa98@gmail.com>
2019-01-27 15:43:45 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Text;
+
+namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxMulti = new System.Windows.Forms.TextBox();
+            this.cboRegion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtDOB = new System.Windows.Forms.DateTimePicker();
+            this.radioM = new System.Windows.Forms.RadioButton();
+            this.radioF = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fileOpenButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxMulti
+            // 
+            this.textBoxMulti.Location = new System.Drawing.Point(12, 89);
+            this.textBoxMulti.Multiline = true;
+            this.textBoxMulti.Name = "textBoxMulti";
+            this.textBoxMulti.Size = new System.Drawing.Size(776, 107);
+            this.textBoxMulti.TabIndex = 0;
+            this.textBoxMulti.TextChanged += new System.EventHandler(this.textBoxMulti_TextChanged);
+            // 
+            // cboRegion
+            // 
+            this.cboRegion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboRegion.FormattingEnabled = true;
+            this.cboRegion.Items.AddRange(new object[] {
+            "삼성",
+            "LG",
+            "Apple"});
+            this.cboRegion.Location = new System.Drawing.Point(119, 52);
+            this.cboRegion.Name = "cboRegion";
+            this.cboRegion.Size = new System.Drawing.Size(121, 20);
+            this.cboRegion.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(119, 16);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(100, 21);
+            this.textName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Company";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dtDOB
+            // 
+            this.dtDOB.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtDOB.Location = new System.Drawing.Point(338, 52);
+            this.dtDOB.Name = "dtDOB";
+            this.dtDOB.Size = new System.Drawing.Size(110, 21);
+            this.dtDOB.TabIndex = 5;
+            this.dtDOB.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // radioM
+            // 
+            this.radioM.AutoSize = true;
+            this.radioM.Location = new System.Drawing.Point(329, 13);
+            this.radioM.Name = "radioM";
+            this.radioM.Size = new System.Drawing.Size(35, 16);
+            this.radioM.TabIndex = 6;
+            this.radioM.TabStop = true;
+            this.radioM.Text = "남";
+            this.radioM.UseVisualStyleBackColor = true;
+            // 
+            // radioF
+            // 
+            this.radioF.AutoSize = true;
+            this.radioF.Location = new System.Drawing.Point(379, 13);
+            this.radioF.Name = "radioF";
+            this.radioF.Size = new System.Drawing.Size(35, 16);
+            this.radioF.TabIndex = 7;
+            this.radioF.TabStop = true;
+            this.radioF.Text = "여";
+            this.radioF.UseVisualStyleBackColor = true;
+            this.radioF.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "저장";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 242);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 131);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop_1);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "파일";
+            // 
+            // fileOpenButton
+            // 
+            this.fileOpenButton.Location = new System.Drawing.Point(129, 401);
+            this.fileOpenButton.Name = "fileOpenButton";
+            this.fileOpenButton.Size = new System.Drawing.Size(75, 23);
+            this.fileOpenButton.TabIndex = 11;
+            this.fileOpenButton.Text = "파일열기";
+            this.fileOpenButton.UseVisualStyleBackColor = true;
+            this.fileOpenButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(240, 401);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "테스트";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.fileOpenButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioF);
+            this.Controls.Add(this.radioM);
+            this.Controls.Add(this.dtDOB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboRegion);
+            this.Controls.Add(this.textBoxMulti);
+            this.Name = "Form1";
+            this.Text = "입력화면";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxMulti;
+        private System.Windows.Forms.ComboBox cboRegion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtDOB;
+        private System.Windows.Forms.RadioButton radioM;
+        private System.Windows.Forms.RadioButton radioF;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button fileOpenButton;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mf = radioM.Checked ? "남" : "여";
+            string data = textName.Text + "," +
+                cboRegion.SelectedItem + "," +
+                dtDOB.Value + "," +
+                mf;
+
+            StreamWriter wr = new StreamWriter(@"F:\Test1.txt", true);
+            wr.WriteLine(data);
+            wr.Close();
+
+            
+            textName.Text = "";
+            cboRegion.SelectedIndex = -1;
+            dtDOB.Value = DateTime.Now;
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxMulti_TextChanged(object sender, EventArgs e)
+        {
+            //textBoxMulti.Text = "hello";
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //StringBuilder sb = new StringBuilder();
+            //sb.AppendLine("이름 : 홍길동");
+            //sb.AppendLine("나이 : 25세");
+            //sb.AppendLine("국적 : 한국");
+            //textBoxMulti.Text = sb.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_DragEnter_1(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy | DragDropEffects.Scroll;
+            }
+
+            //if (e.Data.GetDataPresent(typeof(string)))
+            //{
+            //    // Drop하여 복사함
+            //    e.Effect = DragDropEffects.Copy;
+            //}
+            //else
+            //{
+            //    // Drop 할 수 없음
+            //    e.Effect = DragDropEffects.None;
+            //}
+        }
+
+        private void textBox1_DragDrop_1(object sender, DragEventArgs e)
+        {
+            textBoxMulti.Text = "";
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                this.textBox1.Text = "";
+                string[] file = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string str in file)
+                {
+                    this.textBox1.Text += str;// + "\r" + "\n";
+                    //this.textBox1.Text = str + "\r" + "\n";
+                }
+            }
+
+            //// e.Data.GetData() 메서드는 드래그-앤-드롭에서 전달된 데이타를 가져옴.
+            //// 타켓컨트롤(txtDropTarget)에 드랍 데이타 지정
+            //textBox1.Text = (string)e.Data.GetData(DataFormats.StringFormat);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 데이타를 읽는 StreamReader
+            StreamReader rd = new StreamReader(textBox1.Text);
+
+            // 마지막이 될 때까지 루프
+            while (!rd.EndOfStream)
+            {
+                // 한 라인을 읽는다
+                string line = rd.ReadLine();
+
+                textBoxMulti.Text += line +  "\r" + "\n";
+
+                // 라인을 콤마로 분리하여 컬럼을 만든다
+                //string[] cols = line.Split(',');
+
+                // 한 라인에 각 컬럼의 데이타를 순서대로 넣는다
+                //dataGridView1.Rows.Add(cols[0], cols[1], cols[2], cols[3], cols[4]);
+            }
+
+            // StreamReader는 사용 후 반드시 닫는다
+            rd.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            XmlDocument xdoc = new XmlDocument();
+
+            // XML 데이타를 파일에서 로드
+            // xdoc.Load(@"C:\Temp\Emp.xml"); 
+            xdoc.Load(textBox1.Text);
+            // 특정 노드들을 필터링
+            XmlNodeList nodes = xdoc.SelectNodes("/girlgroup/albums/album");
+
+            foreach (XmlNode emp in nodes)
+            {
+                // Attribute 읽기
+                string order = emp.Attributes["order"].Value;
+
+                string text = emp.InnerText;
+
+                textBoxMulti.Text += order + ", " + text + "\r" + "\n";
+
+                //// 특정 자식 Element 읽기
+                //string name = emp.SelectSingleNode("./Name").InnerText; //Relative Path 사용
+                //string dept = emp.SelectSingleNode("Dept").InnerText;   //간단히 자식 Element명 사용
+                //Console.WriteLine(id + "," + name + "," + dept);
+
+                //// 자식 노드들에 대해 Loop를 도는 예
+                //foreach (XmlNode child in emp.ChildNodes)
+                //{
+                //    Console.WriteLine("{0}: {1}", child.Name, child.InnerText);
+                //}
+            }
+
+            // 특정 Id 속성으로 하나의 Employee 검색 예
+            XmlNode emp1002 = xdoc.SelectSingleNode("/girlgroup/albums/album[@order='EP 3집']");
+            textBoxMulti.Text += emp1002.InnerXml;
+        }
+    }
+}
